ea446d72c9a42f7c2f93789203442f85a1acce8c
Michel Lavezzo <michellavezzo@gmail.com>
2021-07-22 22:02:36 -0300

    componentizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,60 @@
-export function Content() {
+import { useEffect, useState } from "react";
+
+import { Button } from "./Button";
+import { MovieCard } from "./MovieCard";
+
+import "../styles/global.scss";
+
+import "../styles/sidebar.scss";
+import "../styles/content.scss";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface Props {
+  genres: GenreResponseProps[];
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+  selectedGenreId: number;
+}
+
+export function Content(props: Props) {
   // Complete aqui
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {props.selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,43 @@
-export function SideBar() {
+import { useEffect, useState } from "react";
+
+import { Button } from "./Button";
+import { MovieCard } from "./MovieCard";
+
+import "../styles/global.scss";
+
+import "../styles/sidebar.scss";
+import "../styles/content.scss";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface Props {
+  button: JSX.Element[];
+}
+export function SideBar(props: Props) {
   // Complete aqui
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">{props.button}</div>
+    </nav>
+  );
 }
